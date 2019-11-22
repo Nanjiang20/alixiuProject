944daf811221475486efc45ce84f7f7053311562
kws <1046041369@qq.com>
2019-11-22 10:59:48 +0800

    修改端口用户账号-忽略文件清单

--- alibaixiu/app.js
+++ alibaixiu/app.js
@@ -29,11 +29,11 @@ app.use(formidableMiddleware({
 }));
 
 // 数据库连接
-mongoose.connect('mongodb://itcast:itcast@localhost:27017/alibaixiu', { useNewUrlParser: true, useCreateIndex: true})
+mongoose.connect('mongodb://localhost:27017/alibaixiu', { useNewUrlParser: true, useCreateIndex: true})
 	.then(() => console.log('数据库连接成功'))
 	.catch(() => console.log('数据库连接失败'));
 
 // 路由
 require('./routes')(app);
 // 返回系统监听
-app.listen(3000, () => console.log('服务器启动成功'));
+app.listen(80, () => console.log('服务器启动成功'));
